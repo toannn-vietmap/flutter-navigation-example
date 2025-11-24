8b54a8a3532832213264b79c3bccefcdec8dd009
toannn-vietmap <toannn@vietmap.vn>
2025-11-24 18:00:54 +0700

    feat: change config to build android

--- lib/extension/num_extension.dart
+++ lib/extension/num_extension.dart
@@ -35,4 +35,13 @@ extension NumExtension on num? {
       return '${(seconds / 86400).toStringAsFixed(0)} ngày, ${(seconds % 86400 / 3600).toStringAsFixed(0)} giờ, ${(seconds % 3600 / 60).toStringAsFixed(0)} phút';
     }
   }
+
+  String convertSecondsToMinutes() {
+    if (this == null) return '';
+    if (this! < 60) {
+      return '1 giây';
+    } else {
+      return '${(this! / 60).toStringAsFixed(0)} phút';
+    }
+  }
 }
